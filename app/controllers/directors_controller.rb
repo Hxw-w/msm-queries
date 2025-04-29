fff1d9d8a26124efb6cf880a87bb02a1e8a7ccea
Hxw-w <74304447+Hxw-w@users.noreply.github.com>
2025-04-29 23:26:24 +0000

    Added the actors and youngest and eldest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,4 +11,16 @@ class DirectorsController < ApplicationController
     
     render( :template => "director_templates/details")
   end
+  def youngest
+    # Find the youngest director by ordering by date of birth (dob) in descending order, skip those with nil dob
+    # and then take the last one (which will be the youngest).
+    @youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+    
+    render( :template => "director_templates/youngest")
+  end
+  def eldest
+    @eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+    
+    render( :template => "director_templates/eldest")
+  end
 end
